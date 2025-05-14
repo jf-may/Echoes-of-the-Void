3a6210d3c1d0bdb92fb2c6dc8707b05ca298497a
jf-may <juarezferriolmay@proton.me>
2025-05-14 19:27:29 -0300

    Added the item "Void Echolet", texture missing
    Signed-off-by: jf-may <juarezferriolmay@proton.me>

--- src/main/java/may/echoesofthevoid/EchoesOfTheVoid.java
+++ src/main/java/may/echoesofthevoid/EchoesOfTheVoid.java
@@ -1,5 +1,6 @@
 package may.echoesofthevoid;
 
+import may.echoesofthevoid.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class EchoesOfTheVoid implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/may/echoesofthevoid/item/ModItems.java
@@ -0,0 +1,27 @@
+package may.echoesofthevoid.item;
+
+import may.echoesofthevoid.EchoesOfTheVoid;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item VOID_ECHOLET = registerItem("void_echolet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(EchoesOfTheVoid.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        EchoesOfTheVoid.LOGGER.info("Registering Mod Items for " + EchoesOfTheVoid.MOD_ID);
+
+        // TODO: Make a new item group for every item in the mod
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(VOID_ECHOLET);
+        });
+    }
+}
